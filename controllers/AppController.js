8319a8108a325b73cc46e801fc9aa6ce1f8ff1ee
theChosenDevop <tobbey.adesanya@gmail.com>
2024-07-31 13:45:15 +0100

    update files

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,10 +3,17 @@ import dbClient from "../utils/db";
 
 const AppController = {
   async getStatus(req, res) {
-    const redis = redisClient.isAlive();
-    const db = dbClient.isAlive();
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
 
     res.status(200).send({ redis: redisStatus, db: sbStatus });
+  },
+
+  async getStats(req, res) {
+    const userCounts = await dbClient.nbUsers();
+    const fileCounts = await dbClient.nbFiles();
+
+    res.status(200).send({ users: userCounts, files: fileCounts });
   },
 };
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-import express from "express";
-import AppController from "../controllers/AppController";
+import express from 'express';
+import AppController from '../controllers/AppController';
 
 const routes = express.Router();
 
 routes.get("/status", AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
 export default routes;
